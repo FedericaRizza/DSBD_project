10bfaa6ce153b26b3c35c5e2a37afd716e77a65a
Federica Rizza <uni303002@studium.unict.it>
2023-01-16 17:15:08 +0100

    first

--- ETLdatapipeline/ETLDataPipeline.py
+++ ETLdatapipeline/ETLDataPipeline.py
@@ -11,11 +11,11 @@ prom = PrometheusConnect(url="http://15.160.61.227:29090", disable_ssl=True)
 
 
 
-#---------------CALCOLO SET DI METADATI-----------------------------------------------------------------------------------------------
+#%%---------------CALCOLO SET DI METADATI-----------------------------------------------------------------------------------------------
 #prende a partire da ora, la metrica, nei 10 minuti passati, avaibleMem relativa ai nodi con job = summary. Campiona 
 
 label_config = {'job': 'summary'}
-start_time = parse_datetime("5w") 
+start_time = parse_datetime("2w") 
 end_time = parse_datetime("now")
 chunk_size = timedelta(days=1) #??discuterne con gli altri campione? oppure insieme di valori presi in un giorno? dagli appunti: Un chunks è una raccolta di campioni in un intervallo di tempo per una particolare serie.
 
@@ -27,39 +27,81 @@ metric_data = prom.get_metric_range_data(
         chunk_size = chunk_size,
     )
 
-#---------------PUNTO 3: PREDIZIONE DI MAX, MIN, AVG NEI SUCCESSIVI 10MIN---------------------------------------------------------------------
-metric_object_list = MetricsList(metric_data) 
-my_metric_object = metric_object_list[0]
-my_metric_object.metric_values.to_csv('node_filesystem_avail_bytes.csv',index=False)
+#%%---------------PUNTO 3: PREDIZIONE DI MAX, MIN, AVG NEI SUCCESSIVI 10MIN---------------------------------------------------------------------
+metric_object_list = MetricsList(metric_data)
+my_metric_object = metric_object_list[0] #ogni elemento della lista è la serie temporale di un'istanza di job
+my_metric_object.metric_values.to_csv('./ETLDataPipeline/availableMem.csv',index=False)
         #scriviamo i valori calcolati in un file
-ts = pd.read_csv('./node_filesystem_avail_bytes.csv', header=0, parse_dates=[0], dayfirst=True, index_col=0) #leggo il file
-ts.index #stampa i nomi delle righe, che nel caso nostro sono i timestamp
+ts = pd.read_csv('./ETLDataPipeline/availableMem.csv', header=0, parse_dates=[0], dayfirst=True, index_col=0) #leggo il file
+#print("indici: ", ts.index) #stampa i nomi delle righe, che nel caso nostro sono i timestamp
+#print(ts)
+#ts.plot()
 
+#%%----------------DECOMPOSIZIONE----------------------------------------
+
+from statsmodels.tsa.filters.hp_filter import hpfilter
+# Tuple unpacking
+ts_cycle, ts_trend = hpfilter(ts, lamb=1600)  #1600 suggested value that works fine with 
+
+#define size
+plt.figure(figsize=(20,8))
+#add axes labels and a title
+plt.ylabel('Values', fontsize=14)
+plt.xlabel('Time', fontsize=14)
+plt.title('Values over time', fontsize=16)
+plt.plot(ts, "-", label = 'ts')
+plt.plot(ts_trend,"--", label = 'ts_t')
+plt.plot(ts_cycle,".", label = 'ts_c')
+plt.legend(title='Series')
+
+
+from statsmodels.tsa.seasonal import seasonal_decompose
 #esplicito la frequenza perchè non riesce a capire il tempo di campionameto?
-ricampionamento = 60
+ricampionamento = 1
 tsr = ts.resample(rule=str(ricampionamento)+'T').mean()
-tsr.index #vediamo se ci da la frequenza
-tsr.plot() #vediamo graficamente la serie 
-tsr
 
+result= seasonal_decompose(tsr, model='add', period=6)
+result.plot()
+
+#define size
+plt.figure(figsize=(24,10))
+#add axes labels and a title
+plt.ylabel('Values', fontsize=14)
+plt.xlabel('Time', fontsize=14)
+plt.title('Values over time', fontsize=16)
+plt.plot(ts.iloc[:250], "-", label = 'ts')
+plt.plot(result.trend.iloc[:250],"*", label = 'ts_t')
+plt.plot(result.seasonal.iloc[:250],"--", label = 'ts_s')
+plt.plot(result.resid.iloc[:250],".", label = 'ts_err')
+plt.legend(title='Series')
+
+plt.show()
+
+#tsr = ts.resample(rule='T').mean()
+#print("indici resample: ", tsr.index) #vediamo se ci da la frequenza
+#print(tsr)
+#tsr.plot() #vediamo graficamente la serie 
+'''
 print(tsr.size)
 print(int(tsr.size*0.9)) #utilizzo il 90% dei campioni per fare l training
 print((tsr.size-int(tsr.size*0.9)))
-
+'''
 #Split training and test data (80/20% 0r 90/10)
-train_data = tsr.iloc[:int(tsr.size*0.9)]
+train_data = tsr.iloc[:int(tsr.size*0.9)] 
 test_data = tsr.iloc[int(tsr.size*0.9):]
-print(train_data.size)
-print(test_data.size)
+'''print(train_data.size)
+print(test_data.size)'''
 
-test_data
+#test_data
 
 from statsmodels.tsa.holtwinters import ExponentialSmoothing
-tsmodel = ExponentialSmoothing(train_data, trend='add', seasonal='add',seasonal_periods=1440*2/ricampionamento).fit()
+tsmodel = ExponentialSmoothing(train_data, trend='add', seasonal=None).fit()
+#tsmodel
 
-prediction = tsmodel.forecast(1440/ricampionamento)
+prediction = tsmodel.forecast(10)
+#prediction
 
-plt.figure(figsize=(24,10))
+plt.figure(figsize=(20,10))
 #add axes labels and a title
 plt.ylabel('Values', fontsize=14)
 plt.xlabel('Time', fontsize=14)
@@ -71,7 +113,7 @@ plt.plot(prediction,"--",label = 'pred')
 #add legend
 plt.legend(title='Series')
 
-plt.figure(figsize=(24,10))
+plt.figure(figsize=(20,10))
 #add axes labels and a title
 plt.ylabel('Values', fontsize=14)
 plt.xlabel('Time', fontsize=14)
@@ -80,7 +122,7 @@ plt.plot(test_data,"-",label = 'real')
 plt.plot(prediction,"--",label = 'pred')
 #add legend
 plt.legend(title='Series')
-
+'''
 tsmodel = ExponentialSmoothing(tsr, trend='add', seasonal='add',seasonal_periods=1440*2/ricampionamento).fit()
 prediction = tsmodel.forecast(1440/ricampionamento)
 
@@ -96,9 +138,9 @@ plt.plot(prediction,"--",label = 'pred')
 #add legend
 plt.legend(title='Series')
 
-plt.show()
-#----------------PUNTO 2: CALCOLO DEL VALORE DI MAX, MIN, AVG, DEV_STD DELLE METRICHE DI 1h, 3h, 12------------------------
-
+plt.show()'''
+#%%----------------PUNTO 2: CALCOLO DEL VALORE DI MAX, MIN, AVG, DEV_STD DELLE METRICHE DI 1h, 3h, 12h------------------------
+'''
 #TODO:scegliere 10-20 metriche
 metric_set = ["availableMem", "diskUsage"]
 
@@ -138,7 +180,8 @@ for metric in metric_set:
         print("Tipo: ", my_metric_object.metric_values)
         print(type(my_metric_object.metric_values))
         print(type(my_metric_object))
-    '''
+'''
+'''
     metric_data = prom.get_metric_range_data(
         metric_name = i,
         label_config = label_config,
@@ -156,61 +199,3 @@ for metric in metric_set:
     ) 
     
 ''' 
-
-
-    
-'''
-
-ts = pd.read_csv('./node_filesystem_avail_bytes.csv', header=0, parse_dates=[0], dayfirst=True, index_col=0) #leggo il file
-ts.index #stampa i nomi delle righe, che nel caso nostro sono i timestamp
-
-#esplicito la frequenza perchè non riesce a capire il tempo di campionameto?
-ricampionamento = 60
-tsr = ts.resample(rule=str(ricampionamento)+'T').mean()
-tsr.index #vediamo se ci da la frequenza
-tsr.plot() #vediamo graficamente la serie 
-tsr
-
-print(tsr.size)
-print(int(tsr.size*0.9)) #utilizzo il 90% dei campioni per fare l training
-print((tsr.size-int(tsr.size*0.9)))
-
-#Split training and test data (80/20% 0r 90/10)
-train_data = tsr.iloc[:int(tsr.size*0.9)]
-test_data = tsr.iloc[int(tsr.size*0.9):]
-print(train_data.size)
-print(test_data.size)
-
-test_data
-
-from statsmodels.tsa.holtwinters import ExponentialSmoothing
-tsmodel = ExponentialSmoothing(train_data, trend='add', seasonal='add',seasonal_periods=1440*2/60).fit()
-
-prediction = tsmodel.forecast(1440/60)
-
-
-prediction
-
-plt.figure(figsize=(24,10))
-#add axes labels and a title
-plt.ylabel('Values', fontsize=14)
-plt.xlabel('Time', fontsize=14)
-plt.title('Values over time', fontsize=16)
-
-plt.plot(train_data, "-", label = 'train')
-plt.plot(test_data,"-",label = 'real')
-plt.plot(prediction,"--",label = 'pred')
-#add legend
-plt.legend(title='Series')
-
-plt.figure(figsize=(24,10))
-#add axes labels and a title
-plt.ylabel('Values', fontsize=14)
-plt.xlabel('Time', fontsize=14)
-plt.title('Values over time', fontsize=16)
-plt.plot(test_data,"-",label = 'real')
-plt.plot(prediction,"--",label = 'pred')
-#add legend
-plt.legend(title='Series')
-
-'''
